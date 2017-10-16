c06c89a06a35890f4c5143392391d4d641abc9a1
Carlos Pinto <c.pinto@applicaster.com>
2017-10-15 20:26:56 -0400

    chore(middleware): hook up redux-observable middleware for catching actions in epics.

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,20 +1,23 @@
-import { createLogger } from 'redux-logger'
-import { Iterable } from "immutable";
+import { createLogger } from 'redux-logger';
+import { Iterable } from 'immutable';
+import { createEpicMiddleware } from 'redux-observable';
+import rootEpic from '../epics';
 
-const middlewares = [];
+const epicMiddleware = createEpicMiddleware(rootEpic);
+const middlewares = [epicMiddleware];
 
-if (process.env.NODE_ENV !== "production") {
-    const logger = createLogger({
-        stateTransformer: state => {
-            const printableState = {};
-            Object.keys(state).forEach(key => {
-                printableState[key] = Iterable.isIterable(state[key]) ? state[key].toJS() : state[key];
-            });
-            return printableState;
-        },
-    });
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger({
+    stateTransformer: state => {
+      const printableState = {};
+      Object.keys(state).forEach(key => {
+        printableState[key] = Iterable.isIterable(state[key]) ? state[key].toJS() : state[key];
+      });
+      return printableState;
+    },
+  });
 
-    middlewares.push(logger);
+  middlewares.push(logger);
 }
 
 export default middlewares;

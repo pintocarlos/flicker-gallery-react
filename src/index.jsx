27a2202c3b96946f311c88d8e3c4a2022e954160
Carlos Pinto <c.pinto@applicaster.com>
2017-10-16 15:56:20 -0400

    fix(global.scss): renamed global.scss to main.scss

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from 'store';
 import App from './App';
 import './sass/modules/css-reset.scss';
-import './sass/modules/global.scss';
+import './sass/modules/main.scss';
 
 ReactDOM.render(
   <Provider store={ store }>

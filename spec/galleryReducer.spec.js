e380e2e34ba178bcdc27441aa3d4acc9befeabb5
Carlos Pinto <c.pinto@applicaster.com>
2017-10-15 21:04:40 -0400

    feat(galleryReducer): added basic reducer logic for fetching the images data and setting it.

--- spec/galleryReducer.spec.js
+++ spec/galleryReducer.spec.js
@@ -1,5 +1,35 @@
 import { Map } from 'Immutable';
 import galleryReducer from '../src/reducers/galleryReducer';
+import {
+  fetchImagesStart,
+  fetchImagesDone,
+  fetchImagesFailed,
+} from '../src/actions';
+
+const imageSampleData = [
+  {
+    farm: 1,
+    id: 'id1',
+    isfamily: 0,
+    isfriend: 0,
+    ispublic: 1,
+    owner: '126866824@N03',
+    secret: 'secret1',
+    server: 'server1',
+    title: 'IMG_4058-Edit.jpg',
+  },
+  {
+    farm: 2,
+    id: 'id2',
+    isfamily: 0,
+    isfriend: 0,
+    ispublic: 1,
+    owner: '126866824@N03',
+    secret: 'secret2',
+    server: 'server2',
+    title: 'IMG_4058-Edit.jpg',
+  },
+];
 
 describe('galleryReducer', () => {
   const defaultState = Map({
@@ -13,5 +43,56 @@ describe('galleryReducer', () => {
     const currentState = galleryReducer();
 
     expect(currentState.toJS()).to.deep.equal(defaultState.toJS());
+  });
+
+  describe('fetchImagesStart is dispatched', () => {
+    it('should set loading to true', () => {
+      const currentState = galleryReducer().set('loading', false);
+      const action = fetchImagesStart();
+
+      const nextState = galleryReducer(currentState, action);
+
+      expect(nextState.get('loading')).to.be.true;
+    });
+  });
+
+  describe('fetchImagesDone is dispatched', () => {
+    it('should set loading to true', () => {
+      const currentState = galleryReducer();
+      const action = fetchImagesDone();
+
+      const nextState = galleryReducer(currentState, action);
+
+      expect(nextState.get('loading')).to.be.false;
+    });
+
+    it('should set images', () => {
+      const currentState = galleryReducer();
+      const action = fetchImagesDone(imageSampleData);
+
+      const nextState = galleryReducer(currentState, action);
+
+      expect(nextState.get('images')).to.deep.equal({
+        id1: {
+          id: 'id1',
+          url: 'http://farm1.static.flickr.com/server1/id1_secret1.jpg',
+        },
+        id2: {
+          id: 'id2',
+          url: 'http://farm2.static.flickr.com/server2/id2_secret2.jpg',
+        },
+      });
+    });
+  });
+
+  describe('fetchImagesFailed is dispatched', () => {
+    it('should set loading to true', () => {
+      const currentState = galleryReducer();
+      const action = fetchImagesFailed();
+
+      const nextState = galleryReducer(currentState, action);
+
+      expect(nextState.get('loading')).to.be.false;
+    });
   });
 });
--- src/reducers/galleryReducer.js
+++ src/reducers/galleryReducer.js
@@ -1,5 +1,11 @@
 import { Map } from 'immutable';
 import { actionCreator } from '../actions/actionHelpers';
+import {
+  FETCH_IMAGES_START,
+  FETCH_IMAGES_DONE,
+  FETCH_IMAGES_FAILED,
+} from '../actions';
+
 
 const gallery = Map({
   loading: true,
@@ -8,10 +14,40 @@ const gallery = Map({
   searchKey: null,
 });
 
+const composeImageUrl = image =>
+  `http://farm${image.farm}.static.flickr.com/${image.server}/${image.id}_${image.secret}.jpg`;
+
+const processImageData = (imageData) => {
+  const images = {};
+  
+  if (imageData && Array.isArray(imageData)) {
+    for (const image of imageData) {
+      images[image.id] = {
+        id: image.id,
+        url: composeImageUrl(image),
+      };
+    }
+  }
+
+  return images;
+};
+
 export default (state = gallery, action = actionCreator()) => {
   const { type, payload } = action;
 
   switch (type) {
+    case FETCH_IMAGES_START:
+      return state.set('loading', true);
+    
+    case FETCH_IMAGES_DONE:
+      const images = processImageData(payload.images);
+      return state
+        .set('images', images)
+        .set('loading', false);
+
+    case FETCH_IMAGES_FAILED:
+      return state.set('loading', false);
+
     default:
       return state;
   }

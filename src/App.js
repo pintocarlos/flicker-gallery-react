7c18bc9ce83d89702b297765e7ac59b91b265e72
Carlos Pinto <c.pinto@applicaster.com>
2017-10-15 20:28:06 -0400

    feat(App): added basic structure for App component and retrieval of data before rendering component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {
+  fetchImagesStart,
+} from './actions';
 
 class App extends React.Component {
+  componentWillMount() {
+    this.props.fetchImagesStart();
+  }
+  
   render() {
-    return <div>
+    return (<div>
       Setting up for flicker image gallery
-    </div>;
+    </div>);
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-
+  fetchImagesStart,
 }, dispatch);
+
+App.propTypes = {
+  fetchImagesStart: PropTypes.func,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);

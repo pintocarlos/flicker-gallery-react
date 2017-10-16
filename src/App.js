abc13655e9b0cc4e26a63d8178c71d0600c8ede8
Carlos Pinto <c.pinto@applicaster.com>
2017-10-15 22:09:23 -0400

    feat(App): render TeaserBoxes components for the images available.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,50 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
 import {
   fetchImagesStart,
 } from './actions';
 import {
   getUrlForActiveImage,
+  getImageList,
 } from './selectors';
+
 import MainImage from './components/MainImage';
+import TeaserBox from './components/TeaserBox';
 
 class App extends Component {
   componentWillMount() {
     this.props.fetchImagesStart();
   }
   
+  renderTeaserBoxes() {
+    const { imageList = [] } = this.props;
+    const teaserBoxes = [];
+    for (const image of imageList) {
+      const { id, url } = image;
+      teaserBoxes.push(<TeaserBox key={id} id={id} imageUrl={url} />);
+    }
+
+    return teaserBoxes;
+  }
+
   render() {
     const { imageUrl } = this.props;
-    return (<div>
+    const teaserBoxes = this.renderTeaserBoxes();
+
+    return (<div className="app-container">
       <MainImage imageUrl={imageUrl} />
+      <div className="teaser-boxes-container">
+        {teaserBoxes}
+      </div>
     </div>);
   }
 }
 
 const mapStateToProps = state => ({
   imageUrl: getUrlForActiveImage(state),
+  imageList: getImageList(state),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
@@ -33,6 +54,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 
 App.propTypes = {
   fetchImagesStart: PropTypes.func,
+  imageList: PropTypes.array,
   imageUrl: PropTypes.string,
 };
 
